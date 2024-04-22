f3d09e74d227db1d948a93f34dcfa80f3acf08d6
mjsqu <majuqueiroz7@gmail.com>
2024-04-22 17:16:57 -0300

    8 meses fofinhos

---
+++ 8months.py
@@ -0,0 +1,105 @@
+from time import sleep
+print('Oi amor, to juntando todo meu conhecimento- um tanto defasado, to apelando pro youtube também- de informatica '
+      'para te fazer esse projeto. Você é a minha maior sorte e espero que consiga demonstrar um pouquinho da minha'
+      ' felicidade em compartilhar esses 8 meses com você através disso aqui')
+print('[1] Abra quando precisar de uma dose de amor (e não vai ser dose maju)'
+      '\n[2] Abra quando sentir muita saudade'
+      '\n[3] Abra quando estiver triste ou ansioso'
+      '\n[4] Abra quando estivermos brigados'
+      '\n[5] Abra quando esquecer o quão forte você é'
+      '\n[6] ABRA AGORA!!'
+      '\n[7] Sair do Programa')
+opcao = int(input('>>> Qual sua opção: '))
+if opcao == 1:
+    print(f'Se você está precisando de uma dose de amor e eu por algum motivo esteja ocupada para saciar a sua '
+          f'vontade, escolheu a opção certa!'
+          f'Meu amor, amar você é a coisa mais fácil que eu ja tive que fazer, é tão automatico como respirar '
+          f'Você ilumina todos os meus dias, acordar e olhar para você me da gás para enfrentar qualquer coisa nesse'
+          f' mundo. Seu sorriso é a coisa mais linda que existe nesse planeta e eu poderia ficar admirando ele por '
+          f'horas. Um dia eu li que o amor é uma escolha e eu passo e passarei todos os dias da minha vida '
+          f'escolhendo te amar. Você é a minha certeza! Amo o seu toque, o seu beijo, o seu carinho, sua voz, '
+          f'suas palhaçadas, seu jeito, sua companhia, eu amo tudo em você e qualquer coisa que você faça só me faz '
+          f'te amar a cada dia mais. Gostaria de gritar para o mundo o quanto eu te amo em todos os idiomas'
+          f'possiveis, mas por enquanto que so sei o inglês e o português - e um pouco de libras-, estou gritando '
+          f'em python e espero que você consiga sentir de qualquer distância esse amor, porque ele perpassa qualquer '
+          f'barreira ou distância que exista nesse mundo.')
+    print('=-='*10)
+
+
+elif opcao == 2:
+    print(
+        f'Em qualquer distância que estejamos saiba que também estou sentindo muita saudade de você e do seu beijo,'
+        f'seu cheirinho e do seu dengo dengo.'
+        f' Em caso de saudades posso te dar algumas opções de como solucionar ela.'
+        f' Opção número 1: Rua Olavo Batista, 378, Centro - Permitido ir nesse endereço a qualquer momento do dia dar um'
+        f' cheirinho gostoso no motivo da sua saudade para matar esse aperto. Ps: não chamar Jane ou Márcio, talvez o'
+        f' o motivo da sua saudades não atenda o portão.'
+        f' Opção número 2: 73982471874 - Permitido mandar mensagem ou ligar a qualquer momento do dia para ouvir um "eu'
+        f' te amo fofis" do motivo da sua saudades. PS: Se opte por mandar mensagem, corre o risco da resposta ser um'
+        f' um pouco demorada por motivos de: SONINHO DA DIGESTÃO.'
+        f' Opção número 3: Sentir o cheirinho do bodysplash de morango e lembrar da primeira vez que você saiu com o'
+        f' motivo da sua saudades.'
+        f' Opção número 4: Ouvir a playlist que o motivo da sua saudades fez para você e lembrar dela. PS: A playlist é'
+        f' atualizada com frequência, favor instalar spotify e verificar.')
+    print('=-=' * 10)
+
+elif opcao == 3:
+      print(f' Meu amor, eu não sei qual é o motivo da sua tristeza. Talvez tenha sido algo que eu fiz. Não sei ao certo'
+            f', mas independentemente do motivo quero que se lembre do quanto eu te amo e o quanto eu quero que você '
+            f'seja feliz. Se a culpa desse sentimento for minha, me perdoe. A última coisa que quero é te magoar.'
+            f'Lembre-se que nós temos muitos motivos pra sermos felizes e que estamos construindo aos poucos algo muito'
+            f'raro, não somos perfeitos e podemos cometer alguns deslizes no caminho. Então, seja lá o que for que'
+            f'tenha acontecido, lembre-se que você tem a mim. E mais do que sua namorada, eu sou sua amiga.'
+            f'Conte comigo sempre que precisar! Se estiver se sentindo ansioso, vá ao terraço e respire um pouco e se'
+            f'precisar de um abraço bem forte estarei aqui para te acolher. Se precisar chorar estarei aqui para secar'
+            f'suas lágrimas. Estarei sempre aqui nos momentos bons e principalmente nos ruins. '
+            f'Nunca irei soltar a sua mão.')
+      print('=-=' * 10)
+
+elif opcao == 4:
+      print(f'Isso pode não acontecer com frequência, mas infelizmente acontece. Todo casal discute, mas vamos sempre'
+            f'nos lembrar que somos eu e você contra o problema, e jamais um contra o outro. Olha, eu não sei o motivo'
+            f'da nossa briga, não sei de quem foi a culpa ou se existe um culpado, porque geralmente não existe, nossas'
+            f'brigas costumam a ser por mal entendidos. Provavelmente eu estarei com a cabeça a mil e sem querer '
+            f'conversar no momento porque eu preciso do meu tempo, mas eu peço que tenha paciência comigo, eu sei que'
+            f'você já tem muita, mas você sabe que eu não faço por mal e venho tentando diminuir o tempo distante para'
+            f'que possamos resolver o problema o mais rápido possível. Eu sei o quanto eu sou encrenqueira e chata, '
+            f'mas eu também sei o quanto eu te amo e o quanto eu me sinto mal por não conseguir externalizar tão'
+            f'rápido, mas prometo que estou tentando por você e por nós. Você não sabe o quanto eu te acho maravilhoso.'
+            f'Não sabe o quanto você é importante pra mim. Eu não quero te perder nunca. Só de ter você do meu lado eu'
+            f'me sinto a mulher mais feliz desse mundo. E eu não poderia querer mais nada além disso, afinal,'
+            f'você foi a melhor coisa que já me aconteceu. Meu melhor presente. É insubstituível. Eu te amo!')
+      print('=-=' * 10)
+
+elif opcao == 5:
+    print(f'Você é forte meu amor. Muito mais forte do que você imagina! Você já passou por tanta coisa que nem merecia.'
+          f' Passou por muitas fases ruins, tantos momentos horríveis que não gosto nem de imaginar, pois alguém'
+          f' como você deveria viver apenas coisas maravilhosas e se eu pudesse mudar isso, faria sem pensar 2 vezes.'
+          f' Não sei o que está acontecendo que está fazendo você se sentir assim, mas eu sei, que desta vez,'
+          f' você não precisa passar por nada sozinho. Eu estou aqui para o que der e vier! Somos um só agora, '
+          f'seja forte e segure minha mão! Nós vamos juntos nessa e sempre será assim.')
+    print('=-=' * 10)
+
+elif opcao == 6:
+    print(f'Oi meu fofis, feliz 8 meses – espero que eu tenha conseguido terminar isso a tempo – Você merece muito mais'
+          f'do que eu sou capaz de oferecer. Você merece o melhor de mim. Estou disposta a provar todos os dias da'
+          f'minha vida que mereço o seu amor, tanto quanto você merece o meu. Cada dia que passo ao seu lado se torna'
+          f'o melhor da minha vida, apenas porque sei que estou com você. Abrir os olhos e pensar -estou com ele- é a'
+          f'melhor sensação do mundo. Não tenho palavras para agradecer o seu amor por mim. Você me trata como se eu'
+          f'fosse uma princesa e eu não mereço tudo isso. Você me completa e não poderia estar mais feliz por ter você'
+          f'ao meu lado, amor. Você é tudo o que eu sempre imaginei que o amor fosse – e ainda consegue ser melhor.'
+          f'A Maria Júlia criancinha que sonhava com um amor que fosse durar a vida inteira não poderia estar mais'
+          f'feliz porque para minha sorte, hoje não preciso sonhar mais, pois agora, meu sonho já está concretizado.'
+          f'Eu já tenho o meu príncipe, o amor da minha vida, o pai dos meus filhos e a pessoa com quem eu vou passar o'
+          f'resto da vida comigo. Você é tudo para mim e eu não quero te perder nunca. Acima de tudo, sou sua amiga,'
+          f'então não importa o que aconteça, estarei lá para o que você precisar. Eu te amo muito, meu amor!')
+    print('=-=' * 10)
+
+elif opcao  == 0 or opcao > 5:
+        print('Opção Inválida. Tente novamente')
+        print('=-='*10)
+print('Finalizando',end='')
+for c in range(0,5):
+    print('.',end=''), sleep(0.5)
+print('\nFim do Programa! Volte Sempre! TE AMO FOFINHO, TE AMO LINDINHO, TE AMO, TE AMO, TE AMO')
+
